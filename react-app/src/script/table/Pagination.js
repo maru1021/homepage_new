64fa3db8476cae92dad508fb69b5623119450671
maru1021 <poprockguitarmaru@gmail.com>
2025-02-08 15:31:22 +0900

    全体リファクタリング

--- react-app/src/script/table/Pagination.js
+++ react-app/src/script/table/Pagination.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Pagination, Stack } from '@mui/material';
 
+
 const PaginationComponent = ({ totalPages, currentPage, onPageChange }) => {
     return (
         <Stack spacing={2} alignItems='flex-end' sx={{ marginTop: 2 }}>
